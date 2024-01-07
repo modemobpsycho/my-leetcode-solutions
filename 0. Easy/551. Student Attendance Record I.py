364843f4c65877404159c5864094afdd09710cba
Vadim <hawaiianwork@mail.ru>
2024-01-07 20:08:04 +0300

    551. Student Attendance Record I

---
+++ 0. Easy/551. Student Attendance Record I.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+
+        l_cnt = 0
+        a_cnt = 0
+
+        for c in s:
+            if c == 'A':
+                a_cnt += 1
+                if a_cnt == 2:
+                    return False
+
+            if c == 'L':
+                l_cnt += 1
+                if l_cnt > 2:
+                    return False
+            else:
+                l_cnt = 0
+
+        return True
