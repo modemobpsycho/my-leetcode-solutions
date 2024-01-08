8469af13e9c8da7ddf1f724f08256c1774478133
Vadim <hawaiianwork@mail.ru>
2024-01-08 23:06:33 +0300

    66. Plus One

---
+++ 0. Easy/66. Plus One1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        carr = 1
+
+        for i in reversed(range(len(digits))):
+            carr, digits[i] = divmod(digits[i] + carr, 10)
+
+        return digits if not carr else [carr] + digits
