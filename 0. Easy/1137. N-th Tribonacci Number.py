d18d48bf86618d8831d930c6bd5c24ada1099d73
Vadim <hawaiianwork@mail.ru>
2024-01-08 23:30:53 +0300

    1137. N-th Tribonacci Number

---
+++ 0. Easy/1137. N-th Tribonacci Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0, 1, 1]
+
+        if n < 3:
+            return dp[n]
+
+        for i in range(3, n + 1):
+            dp.append(dp[i-3] + dp[i - 2] + dp[i - 1])
+
+        return dp[-1]
