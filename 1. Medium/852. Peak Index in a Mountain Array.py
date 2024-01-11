3cf6fab3b68a3e497fbcbb31d730c44718b3881d
Vadim <hawaiianwork@mail.ru>
2024-01-12 00:55:24 +0300

    852. Peak Index in a Mountain Array

---
+++ 1. Medium/852. Peak Index in a Mountain Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if arr[mid - 1] < arr[mid] > arr[mid + 1]:
+                return mid
+
+            if arr[mid] > arr[mid - 1]:
+                l = mid
+            else:
+                r = mid
