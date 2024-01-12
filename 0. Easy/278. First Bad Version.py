74f54232fc1758e7f615e0e623a279e5125b7bf6
Vadim <hawaiianwork@mail.ru>
2024-01-12 23:41:20 +0300

    278. First Bad Version

---
+++ 0. Easy/278. First Bad Version.py
@@ -0,0 +1,16 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 1, n
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if isBadVersion(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return l
