14f05fdcac40f9d6c0c3caa28553113bebae206c
Vadim <hawaiianwork@mail.ru>
2024-01-14 23:43:49 +0300

    77. Combinations

---
+++ 1. Medium/77. Combinations.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def combinationSum(n):
+    results = []
+    helper(n, [], 1, results)
+    return results
+
+
+def helper(target, combination, start, results):
+    if target == 0:
+        results.append(combination)
+        return
+    if target < 0:
+        return
+
+    for i in range(start, target + 1):
+        helper(target - i, combination + [i], i, results)
+
+
+n = 5
+results = combinationSum(n)
+for combination in results:
+    print(combination)
