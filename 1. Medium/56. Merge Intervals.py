7659532ffcc96fd3d7e7ce7970fd59ea31a6c579
Vadim <hawaiianwork@mail.ru>
2024-01-19 19:50:37 +0300

    56. Merge Intervals

---
+++ 1. Medium/56. Merge Intervals.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda x: x[0])
+
+        ans = []
+
+        for interval in intervals:
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
+            else:
+                ans[-1][1] = max(ans[-1][1], interval[1])
+
+        return ans
