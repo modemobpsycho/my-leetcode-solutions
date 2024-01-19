8c2c7cc012e2a0e47be14541036159a5c8abc07a
Vadim <hawaiianwork@mail.ru>
2024-01-19 22:02:35 +0300

    27. Remove Element

---
+++ 0. Easy/27. Remove Element.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        if nums == []:
+            return 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
