fddc065b5d5144f4304a0fff259c2cac21e1f5ed
Vadim <hawaiianwork@mail.ru>
2024-01-20 17:15:45 +0300

    medium3x

---
+++ 1. Medium/2352. Equal Row and Column Pairs.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        column_starts_with = grid[0]
+
+        columns = [[] for _ in column_starts_with]
+
+        for row in grid:
+            for j, element in enumerate(row):
+                columns[j].append(element)
+
+        equal_pairs = 0
+
+        for row in grid:
+            for j, element in enumerate(column_starts_with):
+                if row[0] == element:
+                    if row == columns[j]:
+                        equal_pairs += 1
+
+        return equal_pairs
