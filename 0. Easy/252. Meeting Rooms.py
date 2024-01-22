7eabd1a53e4ea93cf210c6a298f9855a7a03d6a2
Vadim <hawaiianwork@mail.ru>
2024-01-22 23:17:29 +0300

    updated

--- 0. Easy/252. Meeting Rooms.py
+++ 0. Easy/252. Meeting Rooms.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
 
--- 0. Easy/268. Missing Number.py
+++ 0. Easy/268. Missing Number.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         return list(set(range(len(nums) + 1)) - set(nums))[0]
