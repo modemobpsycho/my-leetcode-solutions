6241d563f0d70db15238bcacfb9aed97846a948e
Vadim <hawaiianwork@mail.ru>
2024-01-23 20:11:59 +0300

    228. Summary Ranges

---
+++ 0. Easy/228. Summary Ranges.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+
+        s: int = nums[0]
+        nums.append(float("inf"))
+        ans = []
+        for i in range(1, len(nums)):
+            if nums[i] - nums[i - 1] > 1:
+                end: int = nums[i - 1]
+                if s == end:
+                    ans.append(str(s))
+                else:
+                    ans.append(f"{s}->{end}")
+
+                s = nums[i]
+
+        return ans
