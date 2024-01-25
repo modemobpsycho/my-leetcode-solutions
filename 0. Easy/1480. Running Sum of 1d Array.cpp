1f0e513e36cc6f50fea79eccc4e9b5a6957e05f8
Vadim <hawaiianwork@mail.ru>
2024-01-25 11:04:26 +0300

    1480. Running Sum of 1d Array

---
+++ 0. Easy/1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int temp = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] += temp;
+            temp = nums[i];
+        }
+        return nums;   
+    }
+};
