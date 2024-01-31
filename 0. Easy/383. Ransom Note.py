0f19ee45ccf049416601500aac115d86a8d5184a
Vadim <hawaiianwork@mail.ru>
2024-01-31 14:16:29 +0300

    383. Ransom Note

---
+++ 0. Easy/383. Ransom Note.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+
+        for i in ransomNote:
+            if i in magazine:
+                magazine.remove(i)
+            else:
+                return False
+        return True
