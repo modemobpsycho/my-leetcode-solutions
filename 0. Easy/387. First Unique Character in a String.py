47b742a1e64a6a04c897a13f51b6a030a73eb076
Vadim <hawaiianwork@mail.ru>
2024-02-05 18:21:15 +0300

    387. First Unique Character in a String

---
+++ 0. Easy/387. First Unique Character in a String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        n: int = len(s)
+        for i in range(n):
+            if s[i] not in s[:i] and s[i] not in s[i+1:]:
+                return i
+        return -1
