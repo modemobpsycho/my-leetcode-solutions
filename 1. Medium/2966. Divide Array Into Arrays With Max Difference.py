fbbc29a9f26b28975d996561900cc4ac63950bf6
Vadim <hawaiianwork@mail.ru>
2024-02-06 22:05:19 +0300

    2966. Divide Array Into Arrays With Max Difference

---
+++ 1. Medium/2966. Divide Array Into Arrays With Max Difference.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        size: int = len(nums)
+        if size % 3 != 0:
+            return []
+        nums.sort()
+
+        result = []
+        group_index = 0
+        for i in range(0, size, 3):
+            if i + 2 < size and nums[i + 2] - nums[i] <= k:
+                result.append([nums[i], nums[i + 1], nums[i + 2]])
+                group_index += 1
+            else:
+                return []
+        return result
